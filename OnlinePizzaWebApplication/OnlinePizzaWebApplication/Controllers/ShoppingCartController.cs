00f412bd77ce9ad986ce821fdc681935643c5b10
devstar0310 <devstar0310@gmail.com>
2022-07-19 17:36:33 +0300

    Update ShoppingCartController.cs

--- OnlinePizzaWebApplication/OnlinePizzaWebApplication/Controllers/ShoppingCartController.cs
+++ OnlinePizzaWebApplication/OnlinePizzaWebApplication/Controllers/ShoppingCartController.cs
@@ -16,6 +16,28 @@ namespace OnlinePizzaWebApplication.Controllers
         private readonly AppDbContext _context;
         private readonly ShoppingCart _shoppingCart;
 
+        public async Task<IActionResult> AddToShoppingCart(int pizzaId)
+        {
+            var selectedPizza = await _pizzaRepository.GetByIdAsync(pizzaId);
+
+            if (selectedPizza != null)
+            {
+                await _shoppingCart.AddToCartAsync(selectedPizza, 1);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> RemoveFromShoppingCart(int pizzaId)
+        {
+            var selectedPizza = await _pizzaRepository.GetByIdAsync(pizzaId);
+
+            if (selectedPizza != null)
+            {
+                await _shoppingCart.RemoveFromCartAsync(selectedPizza);
+            }
+            return RedirectToAction("Index");
+        }
+
         public ShoppingCartController(IPizzaRepository pizzaRepository,
             ShoppingCart shoppingCart, AppDbContext context)
         {
@@ -38,27 +60,6 @@ namespace OnlinePizzaWebApplication.Controllers
             return View(shoppingCartViewModel);
         }
 
-        public async Task<IActionResult> AddToShoppingCart(int pizzaId)
-        {
-            var selectedPizza = await _pizzaRepository.GetByIdAsync(pizzaId);
-
-            if (selectedPizza != null)
-            {
-                await _shoppingCart.AddToCartAsync(selectedPizza, 1);
-            }
-            return RedirectToAction("Index");
-        }
-
-        public async Task<IActionResult> RemoveFromShoppingCart(int pizzaId)
-        {
-            var selectedPizza = await _pizzaRepository.GetByIdAsync(pizzaId);
-
-            if (selectedPizza != null)
-            {
-                await _shoppingCart.RemoveFromCartAsync(selectedPizza);
-            }
-            return RedirectToAction("Index");
-        }
 
         public async Task<IActionResult> ClearCart()
         {
